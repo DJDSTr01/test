1d0329c736ad02adea50b2d7d68fc6c5edac90ac
DJDSTr01 <boudyactivities@gmail.com>
2025-02-05 17:45:24 +0200

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Searching for PcAppstore Virus. . .
+timeout /t 10 /nobreak >nul
+echo VIRUS FOUND. . .
+timeout /t 10 /nobreak >nul
+echo Attempting to Remove. . .
+timeout /t 30 /nobreak >nul
+echo SUCESSFULY REMOVED
+pause
